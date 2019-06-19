924732a6d10433bed65c31e75fa4bf7abb7d38ee
Tehilla Garfinkel <tehillagarfinkel613@gmail.com>
2019-06-18 19:24:51 -0500

    create vehicle class and bike and car to inherit vehicle methods

--- store_item.rb
+++ store_item.rb
@@ -1,22 +1,20 @@
-dress = {color: "pink", price: 34, material: "cotton"}
-shirt = {color: "blue", price: 23, material: "polyester"}
-skirt = {color: "green", price: 19, material: "wool"}
+dress = { color: "pink", price: 34, material: "cotton" }
+shirt = { color: "blue", price: 23, material: "polyester" }
+skirt = { color: "green", price: 19, material: "wool" }
 
 puts "#{dress[:color]} #{dress[:price]} #{dress[:material]}"
 puts "#{shirt[:color]} #{shirt[:price]} #{shirt[:material]}"
 puts "#{skirt[:color]} #{skirt[:price]} #{skirt[:material]}"
 
-
-dress = {"color" => "pink", "price" => 34, "material" => "cotton"}
-shirt = {"color" => "blue", "price" => 23, "material" => "polyester"}
-skirt = {"color" => "green", "price" => 19, "material" => "wool"}
+dress = { "color" => "pink", "price" => 34, "material" => "cotton" }
+shirt = { "color" => "blue", "price" => 23, "material" => "polyester" }
+skirt = { "color" => "green", "price" => 19, "material" => "wool" }
 
 puts "#{dress["color"]} #{dress["price"]} #{dress["material"]}"
 puts "#{shirt["color"]} #{shirt["price"]} #{shirt["material"]}"
 puts "#{skirt["color"]} #{skirt["price"]} #{skirt["material"]}"
 
 class Item
-
   attr_reader :color, :price, :material
   attr_writer :color
 
@@ -29,15 +27,11 @@ class Item
   def print_info
     puts "This is #{@color} and costs #{@price} and is made of #{@material}."
   end
-
 end
 
-dress = Item.new({color: "pink", price: 34, material: "cotton"})
-shirt = Item.new({color: "blue", price: 23, material: "polyester"})
-skirt = Item.new color: "green",price: 19, material: "wool"
+dress = Item.new({ color: "pink", price: 34, material: "cotton" })
+shirt = Item.new({ color: "blue", price: 23, material: "polyester" })
+skirt = Item.new ({ color: "green", price: 19, material: "wool" })
 dress.print_info
 dress.color = "aqua"
 dress.print_info
-
-
-
